ee88628c7d8b35cbb91d25414e25c2043021e160
SevSudERD <161018073+SevSudERD@users.noreply.github.com>
2024-04-29 20:58:21 +0300

    nextjs/tailwind/mongodb
    todo-app

---
+++ Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex py-3 flex-wrap justify-around'>
+      <h1 className='text-lg font-semibold'>Todo APP</h1>
+      <ul className='flex gap-[40px] text-m' >
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/api/route.js
@@ -0,0 +1,43 @@
+import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/TodoModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () =>{
+  await ConnectDB();
+}
+LoadDB();
+
+export async function GET(request){
+  const todos = await TodoModel.find({});
+  return NextResponse.json({todos:todos})
+}
+
+export async function POST(request){
+
+  const {title,description} = await request.json();
+
+  await TodoModel.create({
+    title,
+    description
+  })
+
+  return NextResponse.json({msg:"Todo Created"})
+}
+
+export async function DELETE(request){
+
+  const mongoId = await request.nextUrl.searchParams.get("mongoId");
+  await TodoModel.findByIdAndDelete(mongoId);
+  return NextResponse.json({msg:"Todo Deleted"})
+}
+
+export async function PUT(request){
+
+  const mongoId = await request.nextUrl.searchParams.get("mongoId");
+  await TodoModel.findByIdAndDelete(mongoId,{
+    $set:{
+      isCompleted:true
+    }
+  });
+  return NextResponse.json({msg:"Todo Completed"})
+}
